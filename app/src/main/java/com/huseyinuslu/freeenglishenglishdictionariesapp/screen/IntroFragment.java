c0e85c653f45ad7ba3d2af47ac76f663d802faab
huseyin-uslu <1st.huseyin.uslu@gmail.com>
2021-11-08 20:40:21 +0300

    it can be now found in the google play! Have a good use.

--- app/src/main/java/com/huseyinuslu/freeenglishenglishdictionariesapp/screen/IntroFragment.java
+++ app/src/main/java/com/huseyinuslu/freeenglishenglishdictionariesapp/screen/IntroFragment.java
@@ -35,7 +35,7 @@ public class IntroFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        new CountDownTimer(2500, 1000) {
+        new CountDownTimer(2000, 1000) {
             @Override
             public void onTick(long l) {
 
